bfb8af69ed1a77a4c45f7941fe595fa1dc0b06a6
HyperLeafy <lilesh3016@gmail.com>
2025-11-15 19:36:45 +0530

    Additional bug fixes: issue realting to xss injection

--- src/components/TaskTable.tsx
+++ src/components/TaskTable.tsx
@@ -72,7 +72,7 @@ export default function TaskTable({ tasks, onAdd, onUpdate, onDelete }: Props) {
                           color="text.secondary"
                           noWrap
                           title={t.notes}
-                          dangerouslySetInnerHTML={{ __html: t.notes as unknown as string }}
+                          // dangerouslySetInnerHTML={{ __html: t.notes as unknown as string }}
                         />
                       )}
                     </Stack>
